d126af76b5c16e23fa0724a4de74d089226b4413
Nios-V <niosv.1104@gmail.com>
2025-06-08 16:18:07 -0400

    patients resource template

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { DoctorsModule } from './doctors/doctors.module';
+import { PatientsModule } from './patients/patients.module';
 
 @Module({
-  imports: [DoctorsModule],
+  imports: [DoctorsModule, PatientsModule],
   controllers: [AppController],
   providers: [AppService],
 })
