75ef7c4c619cb0fa55d92bfb09345e7d0798a81c
Nios-V <niosv.1104@gmail.com>
2025-06-08 16:33:02 -0400

    typeORM config + envvars & docker-compose

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,32 @@ import { AppService } from './app.service';
 import { DoctorsModule } from './doctors/doctors.module';
 import { PatientsModule } from './patients/patients.module';
 import { AppointmentsModule } from './appointments/appointments.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
-  imports: [DoctorsModule, PatientsModule, AppointmentsModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: (config: ConfigService) => ({
+        type: 'postgres',
+        host: config.get('DB_HOST'),
+        port: config.get<number>('DB_PORT'),
+        username: config.get('DB_USERNAME'),
+        password: config.get('DB_PASSWORD'),
+        database: config.get('DB_NAME'),
+        autoLoadEntities: true,
+        synchronize: true,
+      }),
+      inject: [ConfigService],
+    }),
+    DoctorsModule,
+    PatientsModule,
+    AppointmentsModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
